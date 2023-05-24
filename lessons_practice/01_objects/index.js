9a60c04d8a4efbb066f26c9c16e1a5a68b579f9e
Konstantin Biliunas <biliunas13f@gmail.com>
2023-05-24 15:48:45 +0300

    fix_lesson_object

--- lessons_practice/01_objects/index.js
+++ lessons_practice/01_objects/index.js
@@ -9,16 +9,22 @@ myCity['country'] = 'USA' // Скобочная запись (Bracket notation)
 // Второй способ добавления используется в случае если необходимо присвоить переменную в значение
 const usaCurrency = 'currency'
 myCity[usaCurrency] = 'dollar'
-console.log(myCity)
+// console.log(myCity) // { city: 'New York', popular: true, country: 'USA', currency: 'dollar' }
 
 // Удаляем свойства из объекта при помощи оператора 'delete'
 delete myCity.popular
 
 // Так же можно добавить вложенное свойство
 myCity.info = { timezone: '+12', timeformat: 'Am/Pm' }
-console.log(myCity.info.timeformat)
+// console.log(myCity.info.timeformat) // Am/Pm
 
-console.log(myCity)
+// console.log(myCity)
+// {
+// 	city: 'New York',
+// 	country: 'USA',
+// 	currency: 'dollar',
+// 	info: { timezone: '+12', timeformat: 'Am/Pm' }
+//   }
 
 // Так же можно собрать объект уже из созданных переменных
 const userName = 'Kostya'
@@ -29,7 +35,7 @@ const userProfile = {
 	postsQty: postsQty,
 	online: false,
 }
-console.log(userProfile)
+// console.log(userProfile) // { userName: 'Kostya', postsQty: 13, online: false }
 
 // Можно использовать сокращенный формат записи свойств, если имя переменной = имени свойств
 const newUserProfile = {
@@ -44,12 +50,50 @@ const newUserProfile = {
 		console.log('Hello')
 	},
 }
-console.log(newUserProfile)
-newUserProfile.status()
-newUserProfile.message()
+// console.log(newUserProfile)
+// {
+// 	userName: 'Kostya',
+// 	postsQty: 13,
+// 	online: false,
+// 	status: [Function: status],
+// 	message: [Function: message]
+//   }
+// newUserProfile.status() // I"m sleeping...
+// newUserProfile.message() // Hello
 
 // Конвертация объекта в JSON формат и обратно
 newObject = JSON.stringify(newUserProfile)
-console.log(newObject)
+// console.log(newObject) // {"userName":"Kostya","postsQty":13,"online":false}
 newObjectTwo = JSON.parse(newObject)
-console.log(newObjectTwo)
+// console.log(newObjectTwo) // { userName: 'Kostya', postsQty: 13, online: false }
+
+// Как избежать мутации объекта, первый способ:
+const person = {
+	name: 'Bob',
+	age: 25,
+	photo: {
+		id: 444,
+		itemsName: 'pro3zz44',
+	},
+}
+const person2 = Object.assign({}, person)
+person2.age = 26
+// console.log(person.age) // 25
+// console.log(person2.age) // 26
+// Второй вариант:
+const person3 = { ...person }
+person3.name = 'Kostya'
+// console.log(person.name) // Bob
+// console.log(person3.name) // Kostya
+
+// Вариант 1 и 2 копирует объект, но если есть вложенные объекты,
+// то копируются ссылки
+person3.photo.id = 555
+// console.log(person.photo.id) // 555
+// console.log(person3.photo.id) // 555
+
+// Полная копия объекта:
+const personCopy = JSON.parse(JSON.stringify(person))
+personCopy.photo.id = 111
+// console.log(person.photo.id) // 555
+// console.log(personCopy.photo.id) // 111
