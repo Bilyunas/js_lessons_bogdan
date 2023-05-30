aed34bfce66b2fb739ebfc4e9837befa258fbf00
Konstantin Biliunas <biliunas13f@gmail.com>
2023-05-30 15:01:05 +0300

    classes

--- lessons_practice/10_classes/index.js
+++ lessons_practice/10_classes/index.js
@@ -6,6 +6,10 @@ class Comment {
 	upvote() {
 		this.voteQty += 1
 	}
+	// создадим новый метод используя static
+	static mergeComments(first, second) {
+		return `${first} ${second}`
+	}
 }
 
 // Создаем новый коментарий используя класс 'Comment'
@@ -19,3 +23,28 @@ firstComment.upvote()
 firstComment.voteQty // 1
 firstComment.upvote()
 firstComment.voteQty // 2
+
+// Проверки принадлежности свойств экземпляров объекта
+firstComment.hasOwnProperty('text') // true
+firstComment.hasOwnProperty('voteQty') // true
+firstComment.hasOwnProperty('upvote') // false
+firstComment.hasOwnProperty('hasOwnProperty') // false
+
+// метод static
+const res = Comment.mergeComments('First text.', 'Second text.')
+res // First text. Second text.
+// static создает метод
+// который доступен как свойство класса Comment
+// и не наследуется экземплярами класса
+
+// Расширение других классов
+class NumbArray extends Array {
+	sum() {
+		return this.reduce((el, acc) => (acc += el), 0)
+	}
+}
+const myArray = new NumbArray(2, 5, 7, 3)
+
+console.log(myArray)
+const res2 = myArray.sum()
+console.log(res2)
